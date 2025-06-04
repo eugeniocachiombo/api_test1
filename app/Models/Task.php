5be7455e90a1978e0fc2894f06d6da4ba740dac3
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-05 00:30:37 +0100

    config relation user in task model

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -11,4 +11,9 @@ class Task extends Model
         "description",
         "user_id"
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class, "user_id", "id");
+    }
+
 }
