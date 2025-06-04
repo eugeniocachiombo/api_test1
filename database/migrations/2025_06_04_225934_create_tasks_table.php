8cd138731573e82a7eb4fed0835f004ca9429e86
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-05 00:04:30 +0100

    configuration tasks migration

--- database/migrations/2025_06_04_225934_create_tasks_table.php
+++ database/migrations/2025_06_04_225934_create_tasks_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string("title");
+            $table->text("description");
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
             $table->timestamps();
         });
     }
