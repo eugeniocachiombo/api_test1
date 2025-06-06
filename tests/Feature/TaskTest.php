5a7ec74d8f20d32ac367fa3419e5c39e2b47044d
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-06 15:54:13 +0100

    Method show in test unit Task is done

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -9,15 +10,17 @@ use Tests\TestCase;
 class TaskTest extends TestCase
 {
 
-    public function test_api_index(){
+    public function test_api_index()
+    {
 
         $response = $this->getJson("api/v1/tasks");
 
         $response->assertStatus(200);
     }
 
-    public function test_api_store(){
-        
+    public function test_api_store()
+    {
+
         $data = [
             "title" => "Test",
             "description" => "nullable",
@@ -27,5 +30,12 @@ class TaskTest extends TestCase
         $response = $this->postJson("api/v1/tasks", $data);
 
         $response->assertStatus(201);
+    }
+
+    public function test_api_show()
+    {
+        $task = Task::find(1);
+        $response = $this->getJson("api/v1/tasks/{$task->id}");
+        $response->assertStatus(200);
     }
 }
