02c2eb4b57806b8bf886477be2917cf483c3bfe1
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-08 15:29:44 +0100

    Swagger: improve server name

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 /**
- * @OA\Server(url="http://127.0.0.1:8000/api/v1")
- * @OA\Info(title="Desafio Api 1", version="0.1")
+ * @OA\Server(url="http://127.0.0.1:8000/api")
+ * @OA\Info(title="Desafio Api 1", version="1.0")
  */
 
 abstract class Controller
