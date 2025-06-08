786803f44072df90638e954c4f5b45c8be942439
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-06-08 15:25:05 +0100

    Swagger: improve server name

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 /**
- * @OA\Server(url="http://localhost/api/v1")
+ * @OA\Server(url="http://127.0.0.1:8000/api/v1")
  * @OA\Info(title="Desafio Api 1", version="0.1")
  */
 
